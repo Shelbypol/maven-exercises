de29d0ff25fa2321d1c37a956e4215d7cb0550fe
Shelby Polasek <shelbypolasek@gmail.com>
2020-06-29 16:26:17 -0500

    finished

---
+++ src/main/java/Practice.java
@@ -0,0 +1,18 @@
+import static org.apache.commons.lang3.StringUtils.*;
+
+public class Practice {
+
+    public static void main(String[] args) {
+//        Tells whether or not what the user entered is a number
+        //isNumeric
+        System.out.println(isNumeric("the"));
+        System.out.println();
+//        Flips the case of the string
+        //swapcase
+        System.out.println(swapCase("The dog's name is Fido."));
+//        Reverses the string
+//Reverse
+        System.out.println(reverse("Life, Universe"));
+
+    }
+}
